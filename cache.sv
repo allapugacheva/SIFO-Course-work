92d1d3c4a1854385aebfeacfbc8c31d7e4bd14eb
allapugacheva <verhovinandrej@gmail.com>
2025-04-28 10:43:25 +0300

    fix cache

--- cache.sv
+++ cache.sv
@@ -2,47 +2,45 @@ module cache #(
 	parameter LINES      = 64,
 				 WORDS      = 32,
 				 DATA_WIDTH = 10,
-				 ADDR_WIDTH = 14,
+				 ADDR_WIDTH = 13,
 				 OFFSET     = $clog2(WORDS),
 				 TAG        = ADDR_WIDTH - OFFSET,
-				 WORDS_LEN  = $clog2(WORDS)
+				 WORDS_LEN  = $clog2(WORDS),
+				 LINES_LEN  = $clog2(LINES)
 ) (
-	input                                                    clk,
-	input                                                    rst,
-	input        [ADDR_WIDTH - 1:0]                          addr,
-	input                                                    read,
-	input                                                    write,
-	input        [DATA_WIDTH - 1:0]                          write_data,
-	output                                                   hit,
-	output       [DATA_WIDTH - 1:0]                          data,
-	output                                                   data_strob,
-	output                                                   ready,
+	input                                       clk,
+	input                                       rst,
+	input        [ADDR_WIDTH - 1:0]             addr,
+	input                                       read,
+	input                                       write,
+	input        [DATA_WIDTH - 1:0]             write_data,
+	output       [DATA_WIDTH - 1:0]             data,
+	output                                      ready,
 	
-	output logic [ADDR_WIDTH - 1:0]                          ram_addr,
-	output logic                                             ram_read,
-	input  logic [DATA_WIDTH - 1:0]                          ram_data_out,
-	output logic                                             ram_write,
-	output logic [DATA_WIDTH - 1:0]                          ram_data_in,
+	output logic [ADDR_WIDTH - 1:0]             ram_addr,
+	output logic                                ram_read,
+	input  logic [DATA_WIDTH - 1:0]             ram_data_out,
+	output logic                                ram_write,
+	output logic [DATA_WIDTH - 1:0]             ram_data_in,
 	
-	output [1 + TAG + WORDS * DATA_WIDTH - 1:0][0:LINES - 1] D_MEM,
-	output [DATA_WIDTH - 1:0]                                D_DATASEL,
-	output [$clog2(WORDS):0]                                 D_DATACNT,
-	output [$clog2(WORDS) - 1:0][0:WORDS - 1]                D_LASTSAVE,
-	output [$clog2(WORDS) - 1:0]                             D_INDEX,
-	output [$clog2(WORDS) - 1:0]                             D_WRITEINDEX
+	output [1 + TAG + WORDS * DATA_WIDTH - 1:0] D_MEM [LINES],
+	output [WORDS_LEN:0]                        D_DATACNT,
+	output [LINES_LEN - 1:0]                    D_LASTSAVE [LINES],
+	output [LINES_LEN - 1:0]                    D_INDEX,
+	output [LINES_LEN - 1:0]                    D_WRITEINDEX
 );
 
 	typedef enum {
 		IDLE,
-		SHOW,
 		READ,
-		LOAD,
-		SAVE
+		LOAD
 	} state_e;
 	
 	state_e state, next_state;
 
-	logic [1 + TAG + WORDS * DATA_WIDTH - 1:0][0:LINES - 1] mem;
+	logic hit;
+	
+	logic [1 + TAG + WORDS * DATA_WIDTH - 1:0] mem [LINES];
 	
 	logic [   TAG - 1:0] tag;
 	logic [OFFSET - 1:0] offset;
@@ -52,30 +50,31 @@ module cache #(
 		offset = addr[    OFFSET - 1:0];
 	end
 	
-	logic [DATA_WIDTH - 1:0]             data_sel;
-	logic [WORDS_LEN:0]                  data_cnt;
+	logic [DATA_WIDTH - 1:0] data_sel;
+	logic [     WORDS_LEN:0] data_cnt;
 	
-	logic [WORDS_LEN - 1:0][0:WORDS - 1] last_save;
-	logic [WORDS_LEN - 1:0]              index;
-	logic [WORDS_LEN - 1:0]              write_index;
+	logic [ LINES_LEN - 1:0] last_save [LINES];
+	logic [ LINES_LEN - 1:0] index;
+	logic [ LINES_LEN - 1:0] write_index;
 	
 	always_comb begin
-		data_sel    = 'z;
-		write_index = 'z;
+		data_sel    =   'z;
+		write_index =   'z;
+		hit         =  1'b0;
 		
-		if (state == IDLE | state == SHOW | state == SAVE) begin
-			if (read) begin
-				for (int i = 0; i < LINES; i++) begin
-					if (tag == mem[i][TAG + WORDS * DATA_WIDTH - 1:WORDS * DATA_WIDTH] & mem[i][TAG + WORDS * DATA_WIDTH]) begin
-						data_sel = mem[i][DATA_WIDTH * (offset + 1) - 1 -: DATA_WIDTH];
-					end
+		if (read) begin
+			for (int i = 0; i < LINES; i++) begin
+				if (tag == mem[i][TAG + WORDS * DATA_WIDTH - 1:WORDS * DATA_WIDTH] & mem[i][TAG + WORDS * DATA_WIDTH]) begin
+					data_sel = mem[i][DATA_WIDTH * (offset + 1) - 1 -: DATA_WIDTH];
+					hit      = 1'b1;
 				end
 			end
-			else if (write) begin		
-				for (int i = 0; i < LINES; i++) begin
-					if (tag == mem[i][TAG + WORDS * DATA_WIDTH - 1:WORDS * DATA_WIDTH] & mem[i][TAG + WORDS * DATA_WIDTH]) begin
-						write_index = WORDS_LEN'(i);
-					end
+		end
+		else if (write) begin		
+			for (int i = 0; i < LINES; i++) begin
+				if (tag == mem[i][TAG + WORDS * DATA_WIDTH - 1:WORDS * DATA_WIDTH] & mem[i][TAG + WORDS * DATA_WIDTH]) begin
+					write_index = LINES_LEN'(i);
+					hit         = 1'b1;
 				end
 			end
 		end
@@ -90,39 +89,21 @@ module cache #(
 		ram_data_in = 'z;
 	
 		case (state)
-			IDLE: if (read) begin
-						if      (data_sel === 'z &  mem[index][TAG + WORDS * DATA_WIDTH])
-							next_state = LOAD;
-						else if (data_sel === 'z & ~mem[index][TAG + WORDS * DATA_WIDTH])
-							next_state = READ;
-						else
-							next_state = SHOW;
-					end
-					else if (write) begin						
-						if      (write_index === 'z &  mem[index][TAG + WORDS * DATA_WIDTH])
-							next_state = LOAD;
-						else if (write_index === 'z & ~mem[index][TAG + WORDS * DATA_WIDTH])
-							next_state = READ;
-						else
-							next_state = SAVE;
-					end
-			SHOW: next_state = IDLE;
+			IDLE: if ((read || write) && ~hit)
+						next_state = mem[index][TAG + WORDS * DATA_WIDTH] ? LOAD : READ;
 			READ: if (data_cnt < WORDS) begin
-						ram_addr = { addr[ADDR_WIDTH - 1:OFFSET], OFFSET'(0) } + data_cnt;
-						ram_read = 1'b1;
+						ram_addr    = { addr[ADDR_WIDTH - 1:OFFSET], OFFSET'(0) } + data_cnt;
+						ram_read    = 1'b1;
 					end
-					else if (read)
-						next_state = SHOW;
 					else
-						next_state = SAVE;
-			SAVE: next_state = IDLE;
+						next_state  = IDLE;
 			LOAD: if (data_cnt < WORDS) begin 
 						ram_addr    = { mem[index][TAG + WORDS * DATA_WIDTH - 1:WORDS * DATA_WIDTH], OFFSET'(0) } + data_cnt;
 						ram_data_in = mem[index][DATA_WIDTH * (data_cnt + 1) - 1 -: DATA_WIDTH];
 						ram_write   = 1'd1;
 					end
 					else
-						next_state = READ;
+						next_state  = READ;
 		endcase
 	end
 	
@@ -138,51 +119,48 @@ module cache #(
 		else if (state == READ) begin
 			if (data_cnt < WORDS)
 				data_cnt <= data_cnt + 1'b1;
+			else
+				data_cnt <= '0;
 		end
-		else if (state == IDLE)
-			data_cnt <= '0;
 	end
 	
 	always_ff @(posedge clk or posedge rst) begin
 		if (rst) begin
-			for (int i = 0; i < LINES; i++) begin
+			for (int i = 0; i < LINES; i++)
 				mem[i] <= '0;
-			end
 		end
 		else if (state == READ) begin
-			if (data_cnt < WORDS)
-				mem[index][DATA_WIDTH * (data_cnt + 1) - 1 -: DATA_WIDTH]   <= ram_data_out;
-			else begin
+			if (data_cnt > 1'b0 && data_cnt < WORDS)
+				mem[index][DATA_WIDTH * data_cnt - 1 -: DATA_WIDTH]         <= ram_data_out;
+			else if (data_cnt == WORDS) begin
+				mem[index][DATA_WIDTH * data_cnt - 1 -: DATA_WIDTH]         <= ram_data_out;
 				mem[index][TAG + WORDS * DATA_WIDTH]                        <= 1'b1;
 				mem[index][TAG + WORDS * DATA_WIDTH - 1:WORDS * DATA_WIDTH] <= tag;
 			end
 		end
-		else if (state == SAVE)
+		else if (write && hit)
 			mem[index][DATA_WIDTH * (offset + 1) - 1 -: DATA_WIDTH] <= write_data;
 	end
 	
 	always_comb begin
-		index = '0;
+		index = 'z;
 		
-		if (read | (write & ~hit)) begin
-			for (int i = 1; i < WORDS; i++) begin
+		if (read || (write && ~hit)) begin
+			for (int i = 1; i < LINES; i++)
 				if (last_save[i] > last_save[index])
-					index = WORDS_LEN'(i);
-			end
+					index = LINES_LEN'(i);
 		end
-		else if (write & hit) begin
+		else if (write && hit)
 			index = write_index;
-		end
 	end
 	
 	always_ff @(posedge clk or posedge rst) begin
 		if (rst) begin
-			for (int i = 0; i < WORDS; i++) begin
-				last_save[i] <= WORDS_LEN'(WORDS - 1 - i);
-			end
+			for (int i = 0; i < LINES; i++)
+				last_save[i] <= LINES_LEN'(LINES - 1 - i);
 		end
-		else if ((state == SHOW & ~hit) | state == SAVE) begin
-			for (int i = 0; i < WORDS; i++) begin
+		else if ((write && hit) || (state == READ && data_cnt == WORDS)) begin
+			for (int i = 0; i < LINES; i++) begin
 				if (last_save[i] < last_save[index])
 					last_save[i] <= last_save[i] + 1'b1;
 			end
@@ -197,13 +175,10 @@ module cache #(
 			state <= next_state;
 	end
 	
-	assign data         = state == SHOW ? data_sel : 'z;
-	assign hit          = (state == SHOW | state == SAVE) && data_cnt < WORDS;
-	assign data_strob   = state == SHOW;
-	assign ready        = state == SHOW | state == SAVE;
+	assign data         = data_sel;
+	assign ready        = (read || write) && hit && data_cnt == '0;
 	
 	assign D_MEM        = mem;
-	assign D_DATASEL    = data_sel;
 	assign D_DATACNT    = data_cnt;
 	assign D_LASTSAVE   = last_save;
 	assign D_INDEX      = index;
